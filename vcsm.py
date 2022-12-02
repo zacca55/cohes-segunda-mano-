b70e37c57c032499d2d5fc3fe162787de3f6e09a
zacca55 <119503021+zacca55@users.noreply.github.com>
2022-12-02 12:55:54 +0100

    Update vcsm.py

--- vcsm.py
+++ vcsm.py
@@ -10,9 +10,13 @@ import seaborn as sns
 
 
 #----------------------------------------------CONFIGURACIÓN DE PÁGINA ----------------------------------------------------------------------
+
 st.set_option('deprecation.showPyplotGlobalUse', False)
 st.set_page_config(page_title="Mercado de Segunda Mano", layout="centered") # después establecer el título de página, su layout e icono 
-
+if  st.session_state['answer'] in realans:
+        answerStat = "correct"
+    elif st.session_state['answer'] not in realans:
+        answerStat = "incorrect"
 
 #---------------------------------------------- COSAS QUE PODEMOS USAR EN TODA NUESTRA APP----------------------------------------------------------------------
 
