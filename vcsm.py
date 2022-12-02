180467d9214b345b369c0556adcbc197537f9039
zacca55 <119503021+zacca55@users.noreply.github.com>
2022-12-02 13:04:21 +0100

    Update vcsm.py

--- vcsm.py
+++ vcsm.py
@@ -13,12 +13,6 @@ import seaborn as sns
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 st.set_page_config(page_title="Mercado de Segunda Mano", layout="centered") # después establecer el título de página, su layout e icono 
-
-if  st.session_state['answer'] in realans:
-    answerStat = "correct"
-elif st.session_state['answer'] not in realans:
-    answerStat = "incorrect"
-
 st.write(st.session_state)
 st.write(answerStat)
    
